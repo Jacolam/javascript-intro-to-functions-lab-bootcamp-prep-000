f86e107026640365607d80967baeb67dde4b3118
hilarious-state-5373 <jacob.lam.work@gmail.com>
2019-02-27 16:44:28 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -16,11 +16,11 @@ function logWhisper(string){
 
 function sayHiToGrandma(string){
   if ( string===string.toLowerCase ){
-    return "I can\'t hear you!"
+    return 'I can\'t hear you!'
   }
    if ( string===string.toUpperCase ){
-    return "YES INDEED"
+    return 'YES INDEED'
   }
    if ( string==="I love you, Grandma" ){
-    return "I Love you, too."
+    return 'I Love you, too.'
    }}
